6734110ba7b7b90f38eacbac267d50fa0de6d601
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-04-11 21:11:42 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
 const generaitor = todo =>{
+const html = `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <span> ${todo}</span>
+  <i class="for fa-trash-alt delete"></i>
+  </li>
+`;
