26a6f6b48fd50d94b4d8ebbe4980c81f58df305a
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-04-13 23:11:03 +0430

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,7 @@ list.addEventListener('click' , e => {
     if(e.target.classList.contains('delete'));
     e.target.parentElement.remove();
 })
+
+const filterTodos = (term) => {
+console.log(list.children);
+}
