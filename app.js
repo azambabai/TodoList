c7f9d484cd89a58bab8184d1ac194853433c8a5c
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-04-13 23:10:35 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 const generaitor = todo =>{
 const html = `
   <li class="list-group-item d-flex justify-content-between align-items-center">
